4aacd26d5fb8b9dccfc56a2f93cde04e0270b5a8
Samarthya <samarthya55@gmail.com>
2021-10-19 12:27:19 +0530

    successful dfs'

--- src/App.js
+++ src/App.js
@@ -2,79 +2,76 @@
 /* eslint-disable no-unused-vars */
 import { useEffect } from 'react';
 import './App.css';
+import Graph from './algorithms.js/dfs';
 
-class Cell {
-  constructor(i, j) {
-    this.i = i;
-    this.j = j;
-    this.weight = 1;
-    this.isWall = false;
-  }
-};
+// class Cell {
+//   constructor(i, j) {
+//     this.i = i;
+//     this.j = j;
+//     this.weight = 1;
+//     this.isWall = false;
+//   }
+// };
 
-class Graph {
-  constructor(rows, columns) {
-    this.rows = rows;
-    this.columns = columns;
-    this.weightArray = [];
+// class Graph {
+//   constructor(rows, columns) {
+//     this.rows = rows;
+//     this.columns = columns;
+//     this.weightArray = [];
 
-    for (let i = 0; i < rows; i++) {
-      this.weightArray.push([]);
-      for (let j = 0; j < columns; j++) {
-        this.weightArray[i].push(1);
-      }
-    }
-  }
+//     for (let i = 0; i < rows; i++) {
+//       this.weightArray.push([]);
+//       for (let j = 0; j < columns; j++) {
+//         this.weightArray[i].push(1);
+//       }
+//     }
+//   }
 
-  isValid(i, j) {
-    return (i >= 0) && (i < this.rows) && (j >= 0) && (j < this.columns);
-  }
+//   isValid(i, j) {
+//     return (i >= 0) && (i < this.rows) && (j >= 0) && (j < this.columns);
+//   }
 
-  display() {
-    this.weightArray[5][5] = 44;
-    for (let i = 0; i < this.rows; i++) {
-      console.log(this.weightArray[i]);
-    }
-  }
+//   display() {
+//     this.weightArray[5][5] = 44;
+//     for (let i = 0; i < this.rows; i++) {
+//       console.log(this.weightArray[i]);
+//     }
+//   }
 
-  dfs(start, end) {
+//   dfs(start, end) {
     
-  }
-};
+//   }
+// };
 
 
 function App() {
   const mygraph = new Graph(25, 50);
-  const traversal_speed = 300;
+  const traversal_speed = 30;
 
   let isMouseDown = false;
-
-  const cellClickHandler = (i, j) => {
-    // not required
-  }
   
   const cellMouseDownHandler = (i, j) => {
-    if(mygraph.weightArray[i][j] === 1) {
-       document.getElementById(`cell_${i}_${j}`).style.backgroundColor = "black";
-       mygraph.weightArray[i][j] = Infinity;
-       mygraph.display();
-    } else {
-     document.getElementById(`cell_${i}_${j}`).style.backgroundColor = "white";
-     mygraph.weightArray[i][j] = 1;
-     mygraph.display();
-    }
+       document.getElementById(`${i}_${j}`).style.backgroundColor = "black";
+       mygraph.setWall(mygraph.cellId(i, j));
+      //  mygraph.display();
+  
+    // else {
+    //  document.getElementById(`cell_${i}_${j}`).style.backgroundColor = "white";
+    //  mygraph.weightArray[i][j] = 1;
+    //  mygraph.display();
+    // }
 
     isMouseDown = true;
   }
 
-  const traverse = (traversal_array) => { // [i, j]
+  const traverse = (traversal_array, color) => { // [i, j]
     let i = 0;
 
     const interval = setInterval(() => {
       if(i === traversal_array.length) {
         clearInterval(interval);
       } else {
-        document.getElementById(`cell_${traversal_array[i][0]}_${traversal_array[i][1]}`).style.backgroundColor = "yellow";
+        document.getElementById(traversal_array[i]).style.backgroundColor = color;
         i++;
       }
     }, traversal_speed);
@@ -86,27 +83,35 @@ function App() {
 
   const cellMouseEnterHandler = (i, j) => {
     if(isMouseDown) {
-      document.getElementById(`cell_${i}_${j}`).style.backgroundColor = "black";
-      mygraph.weightArray[i][j] = Infinity;
-      mygraph.display();
+      document.getElementById(`${i}_${j}`).style.backgroundColor = "black";
+      mygraph.setWall(mygraph.cellId(i, j));
+      // mygraph.display();
     }
   }
 
-  useEffect(() => {
+  // useEffect(() => {
     
-  }, [])
+  // }, [])
+
+  const simulate = () => {
+    const {visited_arr, path} = mygraph.dfs("0_0", "5_5");
+    traverse(visited_arr, 'yellow');
+    setTimeout(() => {
+      traverse(path, 'green')
+    }, visited_arr.length * traversal_speed + 100);
+  }
 
   return (
     <div className="App">
       {
-        mygraph.weightArray.map((row, row_index) => {
+        mygraph.cell_array.map((row, row_index) => {
           return (
             <div className="row" key={row_index}>
               {
                 row.map((cell, column_index) => {
-                  return <div className="cell" id={`cell_${row_index}_${column_index}`} 
+                  return <div className="cell" id={`${row_index}_${column_index}`} 
                   key={column_index} 
-                  onClick={() => cellClickHandler(row_index, column_index)} 
+                  // onClick={() => cellClickHandler(row_index, column_index)} 
                   onMouseDown={() => { cellMouseDownHandler(row_index, column_index)} }
                   onMouseUp={() => { cellMouseUpHandler(row_index, column_index) }}
                   onMouseEnter={() => { cellMouseEnterHandler(row_index, column_index) }}
@@ -119,6 +124,7 @@ function App() {
           )
         })
       }
+      <button onClick={simulate}>Simulate</button>
     </div>
   );
 }
